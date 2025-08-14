fe6626ed2e64204af9eed3a036efbf7ba9a26095
supriyasaloni9 <167563829+supriyasaloni9@users.noreply.github.com>
2025-08-15 00:46:49 +0530

    CodeAdded

--- src/components/authpage.js
+++ src/components/authpage.js
@@ -1,7 +1,15 @@
+import Book from '../Assets/book.svg'
+
 function authpage() {
   return (
-    <div className="authpage border border-2 border-black bg-gray-600 border-primary rounded-lg p-4 m-20 ml-32 mr-32 h-[520px]">
-      Hello Supriya
+    <div className="border border-2 border-black bg-gray-600 border-primary rounded-lg p-4 m-20 ml-[25%] mr-32 h-[520px] w-[50%]">
+     <div className="text-center text-white text-3xl font-serif mt-20">
+      Set up your profile
+      </div> 
+      <div className=" h-20 w-20 border border-[1px] ml-[42%] mt-5 border-black">
+      <img src={Book}/>
+      </div>
+    
     </div>
   );
 }
