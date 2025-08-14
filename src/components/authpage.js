e179449770b3b66555fec9d47ca4c8d9459b8b16
Sanat Shikhar Sinha <shikharsanat@gmail.com>
2025-08-14 23:26:29 +0530

    hello

--- src/components/authpage.js
+++ src/components/authpage.js
@@ -1,7 +1,7 @@
 function authpage() {
   return (
     <div className="authpage border border-2 border-black bg-gray-600 border-primary rounded-lg p-4 m-20 ml-32 mr-32 h-[520px]">
-      
+      Hello Supriya
     </div>
   );
 }
