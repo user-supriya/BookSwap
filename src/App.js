568197afefd98effa3a4a1fe6e8f7c9dcb67769b
supriyasaloni9 <167563829+supriyasaloni9@users.noreply.github.com>
2025-08-14 23:22:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./components/authpage";
+
+function App() {
+  return (
+    <>
+    <Login/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/authpage.js
@@ -0,0 +1,8 @@
+function authpage() {
+  return (
+    <div className="authpage border border-2 border-black bg-gray-600 border-primary rounded-lg p-4 m-20 ml-32 mr-32 h-[520px]">
+      
+    </div>
+  );
+}
+export default authpage;
